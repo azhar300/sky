e57c10318440bce261e809fc19d2202bb61239e6
azhar300 <skylineglobalindustries@gmail.com>
2026-08-16 01:13:17 +0500

    Fix route scroll position

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './catalog-pass.css';
 import './catalog-images.css';
 import './premium-overrides.css';
 
+function ScrollToTop() {
+  const { pathname, search } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname, search]);
+  return null;
+}
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
