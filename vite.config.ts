ab86aa9dadb9f756d7b141dd8b9f8a78581b401c
azhar300 <skylineglobalindustries@gmail.com>
2026-08-16 17:13:07 +0500

    Fix Cloudinary asset URL generation at source

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'skyline-cloudinary-product-paths',
-      transform(code, id) {
-        if (id.endsWith('/src/App.tsx')) {
-          return code.replace(
-            'https://res.cloudinary.com/m2w7btvw/image/upload/f_auto,q_auto/${code}',
-            'https://res.cloudinary.com/m2w7btvw/image/upload/f_auto,q_auto/products/${code}'
-          );
-        }
-        return null;
-      },
-    },
-  ],
+  plugins: [react()],
 });
