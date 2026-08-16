58cfe09596ee943a03275406414476c868f1ac5b
azhar300 <skylineglobalindustries@gmail.com>
2026-08-16 17:32:30 +0500

    Remove recursive catalogue runtime observer causing browser freeze

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, useLocation } from 'react-router-dom';
-import './catalog-runtime-fix';
 import App from './App';
 import './styles.css';
 import './catalog-pass.css';
